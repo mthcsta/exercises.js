f15206f356000e6f527a33ba9dbbde05fcc31faa
Matheus Costa <matheuscostakrenn@gmail.com>
2020-10-28 17:29:39 -0300

    Create IdosoComeAlface.js

---
+++ exercises/IdosoComeAlface.js
@@ -0,0 +1,39 @@
+/***************************************************************** 
+ * PROGRAMA COM HUMOR E TESTE DE RECURSÃO 
+ *
+ * Programa que cria a cadeia alimentar onde 
+ * Alface(🥬) come Idoso(👴), Idoso(👴) come Alface(🥬), 
+ * Alface(🥬) come Idoso(👴), Idoso(👴) come Nada(🏊)
+ *****************************************************************
+*/
+
+class Universo {
+  constructor(name){
+  	this.name = name
+  }
+  come(Tipo) {
+    return this.name + ' come ' + Tipo
+  }
+  toString() {
+    return '🏊';
+  }
+}
+
+const Velho = new Universo('👴')
+const Alface = new Universo('🥬')
+
+// Dado um limite de gerações 
+// Retorne o processo da cadeia alimentar, onde 👴 come 🥬 e 🥬 come 👴
+function passarGeração(Geraçoes) {
+  const atualGeraçao = (Geraçoes % 2) ? Velho : Alface
+  if (Geraçoes == 0) {
+    return atualGeraçao
+  }
+  return atualGeraçao.come(passarGeração(Geraçoes-1)) 
+}
+
+
+// testes
+console.log(passarGeração(1))
+console.log(passarGeração(2))
+console.log(passarGeração(5))
